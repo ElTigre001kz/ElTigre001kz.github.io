285189256888b12f98e69d7ce3e75a08eaa00078
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-03-15 15:17:01 +0600

    Create repeatADS.js

---
+++ repeatADS.js
@@ -0,0 +1,11 @@
+var TimeResetADS = 10; // в секундах
+var linkADS = 'https://eltigre001kz.github.io/player.html'; // ссылка на рекламу
+TimeResetADS *= 1000; // перевод в секунды(не трогать)
+function resetADS()
+{
+for(var a_ads=0;a_ads<document.querySelectorAll(".ads").length;a_ads++)
+{
+document.querySelectorAll(".ads")[a_ads].innerHTML = '<iframe src="'+linkADS+'"></iframe>';
+}
+}
+setInterval(resetADS, TimeResetADS);
