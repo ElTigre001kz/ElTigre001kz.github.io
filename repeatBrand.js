64bdef65b7290eea7b242970d1b6b3c13a218d38
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-03-29 15:09:48 +0600

    Update repeatBrand.js

--- repeatBrand.js
+++ repeatBrand.js
@@ -1,1 +1,11 @@
-
+var TimeResetBranding = 30; // в секундах
+var linkBranding = 'https://eltigre001kz.github.io/Branding.html'; // ссылка на рекламу
+TimeResetBranding *= 1000; // перевод в секунды(не трогать)
+function resetBranding()
+{
+for(var a_branding=0;a_branding<document.querySelectorAll(".branding").length;a_branding++)
+{
+document.querySelectorAll(".branding")[a_branding].innerHTML = '<iframe src="'+linkBranding+'"></iframe>';
+}
+}
+setInterval(resetBranding, TimeResetBranding);
