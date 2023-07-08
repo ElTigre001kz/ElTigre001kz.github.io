d3c13c35961569fe72d14dde817121754b1f8d30
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-07-08 11:41:53 +0600

    Update repeatADS.js

--- repeatADS.js
+++ repeatADS.js
@@ -1,4 +1,4 @@
-var TimeResetADS = 180; // в секундах
+var TimeResetADS = 3600; // в секундах
 var linkADS = 'https://eltigre001kz.github.io/AD.html'; // ссылка на рекламу
 TimeResetADS *= 1000; // перевод в секунды(не трогать)
 function resetADS()
