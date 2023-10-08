257fde40becd76dbf3b1fd10f6132e2314626be7
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-10-09 05:14:01 +0600

    Update loadPlayer.js

--- js/loadPlayer.js
+++ js/loadPlayer.js
@@ -10,7 +10,7 @@
 			  video.width = '100%';
 			  video.height = '100%';
 			  // video.setAttribute('allowFullScreen', '');
-			 video.setAttribute('allow', 'fullscreen');
+			 // video.setAttribute('allow', 'fullscreen');
 			     video.setAttribute('allow', 'autoplay *; fullscreen *');
 			     
 			  document.querySelector('#play').appendChild(video);
